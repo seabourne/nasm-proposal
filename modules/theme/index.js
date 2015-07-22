cfa9347b5602e667b6757b02569b4b2bcbc32236
Mike Reich <mjreich@gmail.com>
2015-07-22 10:26:27 -0700

    Added theme

---
+++ modules/theme/index.js
@@ -0,0 +1,30 @@
+/* 
+* @Author: Mike Reich
+* @Date:   2015-07-22 09:37:35
+* @Last Modified 2015-07-22
+*/
+
+'use strict';
+
+var SitesTemplate = function (app, loaded) {
+  
+  app.on('app.startup.before', () => {
+    app.emit('router.setStatic', __dirname+"/assets", '/template')
+  })
+
+  app.on('templates.gatherTemplates', function(gather) {
+    gather('site.index', _render)
+  })
+
+  var _render = function(opts, callback) {
+    var index = __dirname+"/views/index.ejs"
+
+    app.emit('partial.render.ejs', index, opts, callback)
+  }
+
+  loaded()
+}
+
+module.exports = function(app, loaded) {
+  return new SitesTemplate(app, loaded)
+}
