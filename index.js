c4b0d6792ca5e0d394159865d54c15f26d2e7011
Mike Reich <mjreich@gmail.com>
2015-07-22 10:27:06 -0700

    Added babel for ES6 support

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 /* 
 * @Author: Mike Reich
 * @Date:   2015-07-22 09:18:57
-* @Last Modified 2015-07-22 @Last Modified time: 2015-07-22 09:18:57
+* @Last Modified 2015-07-22
 */
 
 'use strict';
+
+require("babel/register")({
+  stage: 0,
+  // this will ensure that only datadash modules are transpiled
+  ignore: new RegExp("^(.*node_modules/.*)")
+})
 
 var App = require('@nxus/core')
 
