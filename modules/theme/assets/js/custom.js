34a3fb96dab607964ecccf875767a1e02c4f9000
Mike Reich <mjreich@gmail.com>
2015-07-23 16:05:42 -0700

    Connected up stats to real data from keenio

--- modules/theme/assets/js/custom.js
+++ modules/theme/assets/js/custom.js
@@ -27,7 +27,8 @@ function addCommas(nStr)
 
     var client = new Keen({
       projectId: "55afc22296773d39f2a15708",
-      writeKey: "735974e2643dd2ed6456f278c8eb8771e31e4fd21decf5df5c7c90699fd1dc7ff889b9ecd1760e153a18aa0161eeb048719142543971fd99ad044b8cdaecbfde159c7112c9f389fe15344af7ae81ad61761e4c9f1032a6e5f999e176a8b54368c0acaa1dec9c108a0c281a851f2db4fb"
+      writeKey: "735974e2643dd2ed6456f278c8eb8771e31e4fd21decf5df5c7c90699fd1dc7ff889b9ecd1760e153a18aa0161eeb048719142543971fd99ad044b8cdaecbfde159c7112c9f389fe15344af7ae81ad61761e4c9f1032a6e5f999e176a8b54368c0acaa1dec9c108a0c281a851f2db4fb",
+      readKey: "416b862211d745d557d5ddf8b2a6b664eebac54e4df84d78ab709661e63d07ca166fd3f6a35ce9d10237768b4c8e66762b39c7f767526822e4e2ac94e390befd5b57225ed2192cd034fdf0ff120596813359ec404450403b7c69e436711dad019cc39d3af7b93d467fe68df46c7b2780"
     });
 
     var renderStatsPage = function() {
@@ -107,6 +108,168 @@ function addCommas(nStr)
     }
 
     setUserId()
+
+    var getStarts = function() {
+      var query = new Keen.Query("count", {
+        eventCollection: "quizStarted",
+        timezone: "UTC"
+      });
+      
+      client.run(query, function(err, response){
+        if(err) return console.log('err', err)
+        $("#plays_count").html(response.result)
+        
+      });    
+    }
+
+    var getCompletions = function() {
+      var query = new Keen.Query("count", {
+        eventCollection: "quizCompleted",
+        timezone: "UTC"
+      });
+      
+      client.run(query, function(err, response){
+        if(err) return console.log('err', err)
+        $("#completions_count").html(response.result)
+        
+      }); 
+    }
+
+    var getAnswers = function() {
+      var query = new Keen.Query("count", {
+        eventCollection: "answer",
+        groupBy: "correct",
+        timezone: "UTC"
+      });
+      
+      client.run(query, function(err, response){
+        if(err) return console.log('err', err)
+        $("#correct_count").html(_.findWhere(response.result, {correct:true}).result);
+        $("#incorrect_count").html(_.findWhere(response.result, {correct:false}).result);
+      });
+    }
+
+    var getAnswerBreakdown = function() {
+      var answers = []
+      getAnswersForQuestion(1, answers, function(answers) {
+        getAnswersForQuestion(2, answers, function(answers) {
+          getAnswersForQuestion(3, answers, function(answers) {
+            getAnswersForQuestion(4, answers, function(answers) {
+              drawAnswerBreakdown(answers)
+            })
+          })
+        })
+      })
+    }
+
+    var getAnswersForQuestion = function (questionId, answers, cb) {
+      var query = new Keen.Query("count", {
+        eventCollection: "answer",
+        filters: [{"operator":"eq","property_name":"questionId","property_value":questionId}],
+        groupBy: "correct",
+        timezone: "UTC"
+      });
+      
+      client.run(query, function(err, response){
+        if(err) return console.log('err', err)
+        var res = _.map(response.result, function(e) {e.questionId = questionId; return e})
+        answers = answers.concat(res)
+        if(cb) cb(answers)
+      });
+    }
+
+    var drawAnswerBreakdown = function(answers) {
+      console.log('drawing breakdown', answers)
+      var correctData = [];
+      var incorrectData = [];
+      for(var i = 1; i < 5; i++) {
+        var a = _.findWhere(answers, {questionId: i, correct: false})
+        var b = _.findWhere(answers, {questionId: i, correct: true})
+        correctData.push(b.result)
+        incorrectData.push(a.result)
+      }
+      var barChartData = {
+        labels : ["Question 1", "Question 2", "Question 3", "Question 4"],
+        datasets : [
+          {
+            fillColor : "#09afdf",
+            strokeColor : "#09afdf",
+            highlightFill: "#6BD5F4",
+            highlightStroke: "#6BD5F4",
+            label: "Correct",
+            data : correctData
+          },
+          {
+            fillColor : "#FDB45C",
+            strokeColor : "#FDB45C",
+            highlightFill: "#FFC870",
+            highlightStroke: "#FFC870",
+            label: "Incorrect",
+            data : incorrectData
+          },
+        ]
+      }
+      var ctx = document.getElementById("bars-graph").getContext("2d");
+      window.myBar = new Chart(ctx).Bar(barChartData, {
+        //responsive: true,
+        showTooltips: true,
+        multiTooltipTemplate: "<%= datasetLabel %>: <%= value %>",
+        scaleFontColor: "#ccc"
+      });
+    }
+
+    var getDailyStarts = function() {
+      var query = new Keen.Query("count", {
+        eventCollection: "quizStarted",
+        interval: "daily",
+        timeframe: "this_7_days",
+        timezone: "UTC"
+      });
+
+      client.run(query, function(err, response){
+        if(err) return console.log('err', err)
+        var labels = _.map(response.result, function(e) {
+          var date = new Date(e.timeframe.start);
+          return (date.getMonth() + 1) + '/' + date.getDate() + '/' +  date.getFullYear();
+        })
+
+        var values = _.map(response.result, function(e) {
+          return e.value
+        })
+
+        var lineChartData = {
+          labels : labels,
+          datasets : [
+            {
+              label: "Visits",
+              fillColor : "rgba(188,188,188,0.2)",
+              strokeColor : "rgba(188,188,188,1)",
+              pointColor : "rgba(188,188,188,1)",
+              pointStrokeColor : "#fff",
+              pointHighlightFill : "#fff",
+              pointHighlightStroke : "rgba(188,188,188,1)",
+              data : values
+            }
+          ]
+        }
+
+        // Line Charts Initialization
+        var ctx = document.getElementById("lines-graph").getContext("2d");
+        window.newLine = new Chart(ctx).Line(lineChartData, {
+          //responsive: true,
+          bezierCurve : false,
+          scaleFontColor: "#ccc"
+        });
+      });
+    }
+
+    if($(".stats").length > 0) {
+      getStarts();
+      getCompletions();  
+      getAnswers();
+      getAnswerBreakdown();
+      getDailyStarts();
+    }
 
     if($("#correct").length > 0) {
       renderStatsPage()
